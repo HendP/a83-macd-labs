8708446fb053c00bd1d724ae16d38053755970a2
HendP <hendpermana12@Gmail.com>
2019-05-20 13:11:05 +0700

    Update Index.php
    Setup Database in Index.php

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "dicodingwebappserver-henhen.database.windows.net";
+    $user = "henhen";
+    $pass = "Aezakmi13";
+    $db = "db_people";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
